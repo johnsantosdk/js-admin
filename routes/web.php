2fd2e5d558062ec1bc76825266f9d7a89810d29f
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2019-02-05 13:40:23 -0300

    Adição dos componentes do admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.' ], function() {
+	Route::get('/', 		['as' => 'index', 'uses' => 'AdminController@index']);
+});
