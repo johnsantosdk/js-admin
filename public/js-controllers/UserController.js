735fae42ede0fd6a11f2f4e3d8e185dbcfac4cba
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2019-05-31 09:33:55 -0300

    Finalizado a função "excluir"

--- public/js-controllers/UserController.js
+++ public/js-controllers/UserController.js
@@ -57,7 +57,7 @@ class UserController {
 				            <td>${Utils.dateFormat(result._register)}</td>
 				            <td>
 				            	<button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-				                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+				                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
 				            </td>
 						`;
 
@@ -126,7 +126,7 @@ class UserController {
 
 				if (item.name === 'photo' || item.name === 'Nphoto') {
 					
-					return item;
+					return item; 
 
 				}
 
@@ -231,7 +231,7 @@ class UserController {
 	            <td>${Utils.dateFormat(objectUser.register)}</td>
 	            <td>
 	            	<button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-	                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+	                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
 	            </td>
 			`;
 
@@ -244,6 +244,18 @@ class UserController {
 	}
 
 	addEventsTr(tr) {
+
+		tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+			if(confirm("Deseja realmente excluir?")){
+
+				tr.remove();
+
+				this.updateCount();
+
+			}
+
+		});
 
 		tr.querySelector(".btn-edit").addEventListener("click", e => {
 			
